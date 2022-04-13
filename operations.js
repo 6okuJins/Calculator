dd424c8d550be8e16489c7593c0a65a5c89dfbb1
6okuJins <d.ma@mail.utoronto.ca>
2022-04-13 14:55:38 -0400

    Add export line

--- operations.js
+++ operations.js
@@ -4,9 +4,12 @@ let multiply = (x, y) => x * y;
 let divide = (x, y) => x / y;
 
 function operate(x, y, operator) {
+    x = Number(x);
+    y = Number(y);
     return operator == '+' ? add(x,y)
         : operator == '-' ? subtract(x,y)
         : operator == '*' ? multiply(x,y)
         : operator == '/' ? divide(x,y)
         : false;
 }
+export {operate as default};
